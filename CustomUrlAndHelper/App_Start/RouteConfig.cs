42ce915cfc479cb6fee178b1f4b230abdae66f1a
zahidul islam <zahidulislam5594@gmail.com>
2021-01-28 01:00:16 +0600

    add custom url extension method from reuse

--- CustomUrlAndHelper/App_Start/RouteConfig.cs
+++ CustomUrlAndHelper/App_Start/RouteConfig.cs
@@ -14,28 +14,28 @@ namespace CustomUrlAndHelper
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "userList",
-                url: "{lang}/user/Index",
+                name: "UserList",
+                url: "{lang}/{controller}/index",
                 defaults: new { controller = "User", action = "Index", lang="en" }
             );
             routes.MapRoute(
                 name: "UserCreate",
-                url: "{lang}/user/Create",
+                url: "{lang}/{controller}/Create",
                 defaults: new { controller = "User", action = "Create", lang = "en" }
             );
             routes.MapRoute(
                 name: "UserEdit",
-                url: "{lang}/user/edit/{id}",
+                url: "{lang}/{controller}/Edit/{id}",
                 defaults: new { controller = "User", action = "Edit", id = UrlParameter.Optional, lang = "en" }
             );
             routes.MapRoute(
                name: "UserDelete",
-               url: "{lang}/user/delete/{id}",
+               url: "{lang}/{controller}/delete/{id}",
                defaults: new { controller = "User", action = "Delete", id = UrlParameter.Optional, lang = "en" }
            );
             routes.MapRoute(
                name: "UserDetails",
-               url: "{lang}/user/details/{id}",
+               url: "{lang}/{controller}/details/{id}",
                defaults: new { controller = "User", action = "Details", id = UrlParameter.Optional, lang = "en" }
            );
 
---
+++ CustomUrlAndHelper/Helper/URLHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CustomUrlAndHelper.Helper
+{
+    public static class URLHelper
+    {
+        public static string Gets(this UrlHelper helper, string controller)
+        {
+            var routeDictionary = new RouteValueDictionary();
+            routeDictionary.Add("controller", controller);
+
+            var route = helper.RouteUrl("UserList", routeDictionary);
+            return HttpUtility.UrlDecode(route, System.Text.Encoding.UTF8).ToLower();
+        }
+
+        public static string Create(this UrlHelper helper,string controller)
+        {
+            var routeDictionary = new RouteValueDictionary();
+            routeDictionary.Add("controller", controller);
+
+            var route = helper.RouteUrl("UserCreate", routeDictionary);
+            return HttpUtility.UrlDecode(route, System.Text.Encoding.UTF8).ToLower();
+        }
+
+        public static string Edit(this UrlHelper helper, string controller,int id)
+        {
+            var routeDictionary = new RouteValueDictionary();
+            routeDictionary.Add("controller", controller);
+            routeDictionary.Add("id", id);
+
+            var route = helper.RouteUrl("UserEdit", routeDictionary);
+            return HttpUtility.UrlDecode(route, System.Text.Encoding.UTF8).ToLower();
+        }
+        public static string Details(this UrlHelper helper, string controller, int id)
+        {
+            var routeDictionary = new RouteValueDictionary();
+            routeDictionary.Add("controller", controller);
+            routeDictionary.Add("id", id);
+
+            var route = helper.RouteUrl("UserDetails", routeDictionary);
+            return HttpUtility.UrlDecode(route, System.Text.Encoding.UTF8).ToLower();
+        }
+        public static string Delete(this UrlHelper helper, string controller, int id)
+        {
+            var routeDictionary = new RouteValueDictionary();
+            routeDictionary.Add("controller", controller);
+            routeDictionary.Add("id", id);
+
+            var route = helper.RouteUrl("UserDelete", routeDictionary);
+            return HttpUtility.UrlDecode(route, System.Text.Encoding.UTF8).ToLower();
+        }
+    }
+}
