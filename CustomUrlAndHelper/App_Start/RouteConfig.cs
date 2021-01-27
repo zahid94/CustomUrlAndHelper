436c95a073016666548a307ea12b9eeb2a475d13
zahidul islam <zahidulislam5594@gmail.com>
2021-01-27 23:37:16 +0600

    add new custom route

--- CustomUrlAndHelper/App_Start/RouteConfig.cs
+++ CustomUrlAndHelper/App_Start/RouteConfig.cs
@@ -14,6 +14,32 @@ namespace CustomUrlAndHelper
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "userList",
+                url: "{lang}/user/Index",
+                defaults: new { controller = "User", action = "Index", lang="en" }
+            );
+            routes.MapRoute(
+                name: "UserCreate",
+                url: "{lang}/user/Create",
+                defaults: new { controller = "User", action = "Create", lang = "en" }
+            );
+            routes.MapRoute(
+                name: "UserEdit",
+                url: "{lang}/user/edit/{id}",
+                defaults: new { controller = "User", action = "Edit", id = UrlParameter.Optional, lang = "en" }
+            );
+            routes.MapRoute(
+               name: "UserDelete",
+               url: "{lang}/user/delete/{id}",
+               defaults: new { controller = "User", action = "Delete", id = UrlParameter.Optional, lang = "en" }
+           );
+            routes.MapRoute(
+               name: "UserDetails",
+               url: "{lang}/user/details/{id}",
+               defaults: new { controller = "User", action = "Details", id = UrlParameter.Optional, lang = "en" }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
