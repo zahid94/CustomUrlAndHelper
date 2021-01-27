fda257f290dc829787e2e7122159d5991abef1e3
zahidul islam <zahidulislam5594@gmail.com>
2021-01-27 23:17:02 +0600

    Add project files.

---
+++ CustomUrlAndHelper/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using CustomUrlAndHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomUrlAndHelper.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(new List<User>() { new User {Id=1,Name="zahid",Description="check" } });
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            return View(new User { Id = 1, Name = "zahid", Description = "check" });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            return View(new User { Id = 1, Name = "zahid", Description = "check" });
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User user)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int Id)
+        {
+            return View(new User { Id = 1, Name = "zahid", Description = "check" });
+        }
+    }
+}
